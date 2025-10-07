da5711582c3f32cc0858459ae7a1f5c2c6aceec4
Shafhan <shafhan.fa@gmail.com>
2025-10-07 21:17:38 +0700

    fix missing _

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_hello():
     client = app.test_client()
     response = client.get('/')
-    assert response.status.code == 200
+    assert response.status_code == 200
     assert b"Hello from Jenkins MultiBranch Pipeline!" in response.data
